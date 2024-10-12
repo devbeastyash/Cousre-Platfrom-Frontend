0e176a9a3fab0eddd634b9a120c493a3afe42d44
Vishal <vishal.mahato@hcltech.com>
2024-10-12 09:55:39 +0530

    fixed width issue on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Navbar = () => {
   return (
     <>
-      <div className="container h-screen bg-slate-300 text-white font-bold w-screen">
+      <div className="container h-screen bg-slate-300 text-white font-bold w-screen ">
         <div className="navbar flex bg-slate-900 justify-between">
           <div className="navleft flex justify-around gap-5">
             <div>
